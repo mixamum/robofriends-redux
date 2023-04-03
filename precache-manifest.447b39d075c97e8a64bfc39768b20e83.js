3fc0236f8e1073e815943065594442b9b3bd9034
Maxim Untilov <maxim.untilov@cognizant.com>
2023-04-03 14:57:49 -0400

    Updates

---
+++ precache-manifest.447b39d075c97e8a64bfc39768b20e83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bc42a2b75c4b72cc694",
+    "url": "/robofriends-redux/static/css/main.59aa5594.chunk.css"
+  },
+  {
+    "revision": "5bc42a2b75c4b72cc694",
+    "url": "/robofriends-redux/static/js/main.5bc42a2b.chunk.js"
+  },
+  {
+    "revision": "674f9ec32734e4bc4ad8",
+    "url": "/robofriends-redux/static/css/1.12795de0.chunk.css"
+  },
+  {
+    "revision": "674f9ec32734e4bc4ad8",
+    "url": "/robofriends-redux/static/js/1.674f9ec3.chunk.js"
+  },
+  {
+    "revision": "9e0b58d846368f426a94",
+    "url": "/robofriends-redux/static/js/runtime~main.9e0b58d8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "90ae956d85c64a10381780571afb4f89",
+    "url": "/robofriends-redux/index.html"
+  }
+];
